13b0d877a90456c2ef60c29321d541f56fc569c3
Falcommander <63583307+Falcommander@users.noreply.github.com>
2021-02-24 14:48:26 +0100

    commit

--- projet.py
+++ projet.py
@@ -1,14 +1,24 @@
 import retro
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import SubprocVecEnv
+import torch
+
+
+
 
 def main():
-    env = retro.make(game='Celeste-GBA', state='Level1')
-    obs = env.reset()
-    while True:
-        obs, rew, done, info =  env.step(env.action_space.sample())
-        env.render()
-        if done:
-            obs = env.reset()
-    env.close()
+    env = SubprocVecEnv([lambda: retro.make(game='Celeste-GBA', state='Level1', scenario='scenario', record="records")])
+    model = PPO('CnnPolicy', env, verbose=1)    
+    model.learn(
+        total_timesteps=int(100e6), 
+        callback=None, 
+        log_interval=1000,
+        tb_log_name='PPO2',
+        reset_num_timesteps=True    
+    )
+    
+
+   
 
 
 if __name__ == "__main__":
